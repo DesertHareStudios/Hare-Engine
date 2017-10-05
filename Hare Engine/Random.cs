5c70984322270a74b953c50559a554a70b13a135
theWildSushii <sushiiito123@gmail.com>
2017-10-04 21:33:01 -0600

    Added random functions (literally...)

---
+++ Hare Engine/Random.cs
@@ -0,0 +1,28 @@
+﻿namespace HareEngine {
+
+    class Random {
+
+        private static int x = 1618;
+
+        public static float Value {
+            get {
+                x = (314 * x + 7) % 1618;
+                return (float)x / 1617f;
+            }
+        }
+
+        public static float Range(float min, float max) {
+            return (Value * (max - min)) + min;
+        }
+
+        public static bool Probably(float probability) {
+            return Value <= probability;
+        }
+
+        public static bool Probably() {
+            return Probably(1f / 2f);
+        }
+
+    }
+
+}
