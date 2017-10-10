a30f31a4532761f56425549fd4084bcabbd60201
theWildSushii <sushiiito123@gmail.com>
2017-10-09 23:55:54 -0600

    Replaced Octonion with Quaternion

--- Hare Engine/Transform/Transform.cs
+++ Hare Engine/Transform/Transform.cs
@@ -3,12 +3,12 @@
 namespace HareEngine {
 
     class Transform {
-        
+
         public GameObject gameObject;
         public Transform parent;
         public List<Transform> childs;
         public Vector position;
-        public Octonion rotation;
+        public Quaternion rotation;
         public Vector scale;
 
         public Transform(GameObject gameObject) {
@@ -16,24 +16,20 @@ namespace HareEngine {
             parent = null;
             childs = new List<Transform>();
             position = new Vector();
-            rotation = new Octonion();
-            scale = new Vector(1f, 1f, 1f, 1f);
+            rotation = new Quaternion();
+            scale = new Vector(1f, 1f, 1f);
         }
 
         public void Translate(Vector to) {
             position += to;
         }
 
-        public void Translate(float x, float y, float z, float w) {
-            position += new Vector(x, y, z, w);
-        }
-
         public void Translate(float x, float y, float z) {
-            position += new Vector(x, y, z, 0f);
+            position += new Vector(x, y, z);
         }
 
         public void Translate(float x, float y) {
-            position += new Vector(x, y, 0f, 0f);
+            position += new Vector(x, y, 0f);
         }
 
     }
